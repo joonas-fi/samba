fee1b7ec402cb018cdafd0d90dfb119c14e6b40e
Joonas Loppi <joonas@joonas.fi>
2019-08-19 15:28:09 +0300

    Make more configurable and auto-export network shares

--- README.md
+++ README.md
@@ -7,7 +7,18 @@ Samba, contained - as all things should be.
 Status
 ------
 
-Not ready for public consumption (yet?).
+Might be ready for public consumption, but don't expect much support if you encounter problems.
+
+
+Use case
+--------
+
+We only support this use case:
+
+The container automatically shares all directories from `/samba-private` to your named user only.
+Only one username/password is supported.
+
+`/samba-public` is writable to guest users (e.g. your family).
 
 
 Running
@@ -17,9 +28,11 @@ Example:
 
 ```
 $ docker run --rm -it -p 445:445 \
-	-e "JOONAS_PASSWORD=..." \
-	-v "/samba/joonas:/samba/joonas" \
-	-v "/samba/public:/samba/public" \
+	-e "SMB_USERNAME=joonas" \
+	-e "SMB_PASSWORD=..." \
+	-v "/home/joonas/music:/samba-private/music" \
+	-v "/home/joonas/movies:/samba-private/movies" \
+	-v "/home/joonas/family-public:/samba-public" \
 	joonas/samba
 ```
 
--- run.sh
+++ run.sh
@@ -1,5 +1,20 @@
 #!/bin/sh -eu
 
+# thanks https://serverfault.com/a/649619
+addSambaShare() {
+	local sharename="$1"
+	local path="$2"
+
+	# [joonas]
+	#   path = /samba-private/joonas
+	#   browseable = yes
+	#   read only = no
+	#   valid users = joonas
+	echo -e "[$sharename]\n  path = $path\n  browseable = yes\n  read only = no\n  valid users = $SMB_USERNAME\n" > "/etc/samba/smb.d/$sharename.conf"
+
+	echo "include = /etc/samba/smb.d/$sharename.conf" >> /etc/samba/includes.conf
+}
+
 addSambaUser() {
 	local username="$1"
 	local password="$2"
@@ -9,10 +24,21 @@ addSambaUser() {
 	echo -e "$password\n$password" | smbpasswd -s -a "$username"
 }
 
-addSambaUser "joonas" "$JOONAS_PASSWORD"
+mkdir -p /etc/samba/smb.d
+echo "" > /etc/samba/includes.conf
+
+directories=/samba-private/*
+for directory in $directories
+do
+	directoryBasename=$(basename "$directory")
+
+	addSambaShare "$directoryBasename" "$directory"
+done
+
+addSambaUser "$SMB_USERNAME" "$SMB_PASSWORD"
 
 # for security, let's unset the ENV var containing secrets
-unset "JOONAS_PASSWORD"
+unset "SMB_PASSWORD"
 
 # for --no-process-group see https://stackoverflow.com/questions/49357524/docker-alpine-samba-does-not-start
 exec smbd --foreground --no-process-group --log-stdout
